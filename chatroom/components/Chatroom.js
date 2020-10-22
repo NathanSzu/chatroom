053e3ddc758feb05fd14bc901086af07c3958275
Nathan Szurek <nathan.szurek@yahoo.com>
2020-10-22 13:31:35 -0500

    Created Chatroom component

---
+++ chatroom/components/Chatroom.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Chatroom() {
+    // Create a reference to the messages firestore collection
+    const messagesRef = firestore.collection('messages');
+    // Make a query for a subset of documents ordered by creation date/time and limited to 25 messages
+    const query = messagesRef.orderBy('createdAt').limit(25);
+
+    // Listen to data and update in real time using useCollectionData hook that we import
+    // Returns an object containing the messages from the database
+    const [messages] = useCollectionData(query, {idField: 'id'});
+
+    // populate the messages using a ChatMessage component and the map method
+    return (
+        <div>
+            {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+        </div>
+    )
+}
