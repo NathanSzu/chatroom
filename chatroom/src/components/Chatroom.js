d8d948e19692e22cac0523a2701930b3a4048b95
Nathan Szurek <nathan.szurek@yahoo.com>
2020-10-27 09:52:06 -0500

    Added css and fixed retrieval to limit to last 25 messages

--- chatroom/src/components/Chatroom.js
+++ chatroom/src/components/Chatroom.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import ChatMessage from './ChatMessage'
 import firebase from 'firebase/app';
 
@@ -7,15 +7,16 @@ export default function Chatroom({ firestore, useCollectionData, auth }) {
     // Create a reference to the messages firestore collection
     const messagesRef = firestore.collection('messages');
     // Make a query for a subset of documents ordered by creation date/time and limited to 25 messages
-    const query = messagesRef.orderBy('createdAt').limit(25);
+    const query = messagesRef.orderBy('createdAt').limitToLast(25);
 
     // Listen to data and update in real time using useCollectionData hook that we import
     // Returns an object containing the messages from the database
     const [messages] = useCollectionData(query, {idField: 'id'});
-    console.log(messages)
 
     // Adding a state to hold message content.
     const [formValue, setFormValue] = useState('');
+
+    const dummy = useRef()
 
     // Event handler to add the message to firestore database when form is submitted.
     const sendMessage = async(e) => {
@@ -33,14 +34,19 @@ export default function Chatroom({ firestore, useCollectionData, auth }) {
 
         setFormValue('');
 
+        // Always scrolls to the referenced div when a new message is sent
+        dummy.current.scrollIntoView({ behavior: 'smooth' })
+
     }
 
     // populate the messages using a ChatMessage component and the map method
     return (
         <>
-            <div>
+            <main>
                 {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} uid={msg.uid} auth={auth} />)}
-            </div>
+
+                <div ref={dummy}></div>
+            </main>
 
             <form onSubmit={sendMessage}>
 
